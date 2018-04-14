f78f53be579c5a8a9f721ea0c632c3363617613c
Wayne <38371589+wj0nes@users.noreply.github.com>
2018-04-13 23:35:02 -0700

    Use random16 for strings longer than 255 pixels

--- lightnings/lightnings.ino
+++ lightnings/lightnings.ino
@@ -55,25 +55,25 @@ void setup() {
 
 void loop() {
   
-  ledstart = random8(NUM_LEDS);                               // Determine starting location of flash
-  ledlen = random8(NUM_LEDS-ledstart);                        // Determine length of flash (not to go beyond NUM_LEDS-1)
+  ledstart = random16(NUM_LEDS);                               // Determine starting location of flash
+  ledlen = random16(NUM_LEDS-ledstart);                        // Determine length of flash (not to go beyond NUM_LEDS-1)
   
-  for (int flashCounter = 0; flashCounter < random8(3,flashes); flashCounter++) {
+  for (int flashCounter = 0; flashCounter < random16(3,flashes); flashCounter++) {
     if(flashCounter == 0) dimmer = 5;                         // the brightness of the leader is scaled down by a factor of 5
-    else dimmer = random8(1,3);                               // return strokes are brighter than the leader
+    else dimmer = random16(1,3);                               // return strokes are brighter than the leader
     
     fill_solid(leds+ledstart,ledlen,CHSV(255, 0, 255/dimmer));
     FastLED.show();                       // Show a section of LED's
-    delay(random8(4,10));                                     // each flash only lasts 4-10 milliseconds
+    delay(random16(4,10));                                     // each flash only lasts 4-10 milliseconds
     fill_solid(leds+ledstart,ledlen,CHSV(255,0,0));           // Clear the section of LED's
     FastLED.show();
     
     if (flashCounter == 0) delay (150);                       // longer delay until next flash after the leader
     
-    delay(50+random8(100));                                   // shorter delay between strokes  
+    delay(50+random16(100));                                   // shorter delay between strokes  
   } // for()
   
-  delay(random8(frequency)*100);                              // delay between strikes
+  delay(random16(frequency)*100);                              // delay between strikes
   
 } // loop()
 
